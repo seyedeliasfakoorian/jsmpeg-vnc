737f44abb0f4187d7c8a254606841c461399850f
Elias Fakoorian <98291494+seyedeliasfakoorian@users.noreply.github.com>
2024-03-23 09:25:12 -0700

    bug fixes

--- source/app.c
+++ source/app.c
@@ -115,12 +115,12 @@ int app_on_http_req(app_t *self, libwebsocket *socket, char *request) {
 		libwebsockets_serve_http_file(self->server->context, socket, "client/index.html", "text/html; charset=utf-8", NULL);
 		return true;
 	}
-	else if( strcmp(request, "/jsmpg.m") == 0 ) {
-		libwebsockets_serve_http_file(self->server->context, socket, "client/jsmpg.m", "text/javascript; charset=utf-8", NULL);
+	else if( strcmp(request, "/jsmpg.js") == 0 ) {
+		libwebsockets_serve_http_file(self->server->context, socket, "client/jsmpg.js", "text/javascript; charset=utf-8", NULL);
 		return true;
 	}
-	else if( strcmp(request, "/jsmpg-vnc.m") == 0 ) {
-		libwebsockets_serve_http_file(self->server->context, socket, "client/jsmpg-vnc.m", "text/javascript; charset=utf-8", NULL);
+	else if( strcmp(request, "/jsmpg-vnc.js") == 0 ) {
+		libwebsockets_serve_http_file(self->server->context, socket, "client/jsmpg-vnc.js", "text/javascript; charset=utf-8", NULL);
 		return true;
 	}
 	return false;
